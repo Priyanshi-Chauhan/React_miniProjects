bf5f7db796fe59c869dcc37a7b625f1414235ad4
Priyanshi-Chauhan <priyanshichauhanx06@gmail.com>
2019-11-28 15:20:08 +0530

    shopping cart

---
+++ reacts/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return (<div>priyanshi</div>);
+  }
+}
+ 
+export default App;
