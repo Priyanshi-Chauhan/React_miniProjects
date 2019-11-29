86a01a74883211a2d9ce5c86f8907917e0dbbf46
Priyanshi-Chauhan <priyanshichauhanx06@gmail.com>
2019-11-29 18:20:49 +0530

    scratch

--- reacts/src/index.js
+++ reacts/src/index.js
@@ -1,2 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+const element = <h1>hello world!</h1>;
+ReactDOM.render(element,document.getElementById('root'));
