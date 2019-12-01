f6d0451f0e868be67db9b0907446cb82a9651dd9
Priyanshi-Chauhan <priyanshichauhanx06@gmail.com>
2019-12-01 14:32:30 +0530

    css bootstrap

--- reacts/src/comp/Counter.jsx
+++ reacts/src/comp/Counter.jsx
@@ -3,13 +3,15 @@ class Counter extends Component {
     state = {
         count:0,
         imageurl:"https://picsum.photos/200"};
-        styles={fontSize:50,
-            fontWeight:"bold"}
+    
     render() { 
-        return(
+         let classes = "badge m-2 badge-";
+         classes+= this.state.count===0 ? "warning" : "primary";
+         
+         return(
         <div>
         <img src={this.state.imageurl} alt=""></img>
-         <span style={this.styles}>{this.format()}</span>
+         <span className={classes}>{this.format()}</span>
          <button className = "btn btn-secondary btn-sm2">increment</button></div>)
     };
     format() {
