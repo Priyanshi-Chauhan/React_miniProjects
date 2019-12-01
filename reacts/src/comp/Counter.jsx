f18e6f028d237378df166546bfd1446e50996526
Priyanshi-Chauhan <priyanshichauhanx06@gmail.com>
2019-12-01 14:20:12 +0530

    internal css

--- reacts/src/comp/Counter.jsx
+++ reacts/src/comp/Counter.jsx
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count:0,
-        imageurl:"https://picsum.photos/200"
-};
+        imageurl:"https://picsum.photos/200"};
+        styles={fontSize:50,
+            fontWeight:"bold"}
     render() { 
         return(
         <div>
         <img src={this.state.imageurl} alt=""></img>
-         <span className= "badge badge-primary m-2">{this.format()}</span>
+         <span style={this.styles}>{this.format()}</span>
          <button className = "btn btn-secondary btn-sm2">increment</button></div>)
     };
     format() {
