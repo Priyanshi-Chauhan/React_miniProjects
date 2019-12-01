62d79366975f15cb15d4e3316fdbee5b68662f6e
Priyanshi-Chauhan <priyanshichauhanx06@gmail.com>
2019-12-01 16:14:48 +0530

    yes

--- reacts/src/comp/Counter.jsx
+++ reacts/src/comp/Counter.jsx
@@ -5,15 +5,20 @@ class Counter extends Component {
         imageurl:"https://picsum.photos/200"};
     
     render() { 
-         let classes = "badge m-2 badge-";
-         classes+= this.state.count===0 ? "warning" : "primary";
+         
          
          return(
         <div>
         <img src={this.state.imageurl} alt=""></img>
-         <span className={classes}>{this.format()}</span>
+         <span className={this.newMethod()}>{this.format()}</span>
          <button className = "btn btn-secondary btn-sm2">increment</button></div>)
     };
+    newMethod() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
     format() {
         const {count} =this.state;
         return count === 0 ? 'ZERo':count;
