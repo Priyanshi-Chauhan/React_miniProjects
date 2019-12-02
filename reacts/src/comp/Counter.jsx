96afb4a9f2dc0b0c0469efd92dc12fe72c21c17e
Priyanshi-Chauhan <priyanshichauhanx06@gmail.com>
2019-12-02 09:12:19 +0530

    arrow function use --very easy

--- reacts/src/comp/Counter.jsx
+++ reacts/src/comp/Counter.jsx
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
 class Counter extends Component {
     state = {
-        count:0,
-        imageurl:"https://picsum.photos/200"};
-    
-    render() { 
-         
-         
-         return(
-        <div>
-        <img src={this.state.imageurl} alt=""></img>
-         <span className={this.newMethod()}>{this.format()}</span>
-         <button className = "btn btn-secondary btn-sm2">increment</button></div>)
-    };
-    newMethod() {
-        let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
-        return classes;
-    }
+        count:0};
+      //  constructor() {
+       //     super();
+         //   this.handleIncrement=this.handleIncrement.bind(this)
 
-    format() {
-        const {count} =this.state;
-        return count === 0 ? 'ZERo':count;
+        //}
+        handleIncrement=()=> {
+            console.log("increment clicked",this);
+        }
+        render() {
+            return (
+                <div><button onClick={this.handleIncrement} className="btn btn-primary sm-2">Increment</button>
+                     </div>
+            )
+        };
     }
-}
  
 export default Counter;
