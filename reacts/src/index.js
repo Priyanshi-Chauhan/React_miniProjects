60be5ece406c0438afb94f691b3e621003ebb1a5
Priyanshi-Chauhan <priyanshichauhanx06@gmail.com>
2019-12-03 09:34:57 +0530

    from scratch

--- reacts/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './comp/Counter';
-import App from './app';
-const element = <h1>hello world!</h1>;
-ReactDOM.render(<Counter><App /></Counter>,document.getElementById('root'));
