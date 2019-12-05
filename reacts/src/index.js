f6c44da4dce5bbf2efcc25fbbb14432ece79f3a8
Priyanshi-Chauhan <priyanshichauhanx06@gmail.com>
2019-12-05 20:00:41 +0530

    starting from scratch

--- reacts/src/index.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./app";
-import * as serviceworker from "./serviceworker";
-ReactDOM.render(<App />,document.querySelector("#root"));
