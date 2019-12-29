c3a5ac6898ad7239dc743fb9967721d01525c924
Priyanshi-Chauhan <priyanshichauhanx06@gmail.com>
2019-12-29 17:05:57 +0530

    rendering list items

--- reacts/src/comp/Counter.jsx
+++ reacts/src/comp/Counter.jsx
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: "http://picsum.photos/200"
+    imageUrl: "http://picsum.photos/200",
+    tags: []
   };
   render() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    let classes = this.getBadgeClasses();
     return (
       <div>
         <img src={this.state.imageUrl} alt=""></img>
@@ -14,12 +14,29 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">increment</button>
+        {this.renderTags()}
       </div>
     );
   }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "ZERO" : count;
+  }
+  renderTags() {
+    if ((this.state.tags.length === 0)) return <p>there are no tags</p>;
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
   }
 }
 
