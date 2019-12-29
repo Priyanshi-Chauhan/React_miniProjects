7e540c95e1ef468815b6464590b320a188d90431
Priyanshi-Chauhan <priyanshichauhanx06@gmail.com>
2019-12-29 15:02:06 +0530

    simple changes

--- reacts/src/comp/Counter.jsx
+++ reacts/src/comp/Counter.jsx
@@ -6,10 +6,14 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.state.count}</span>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">increment</button>
       </div>
     );
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "ZERO" : count;
   }
 }
 
