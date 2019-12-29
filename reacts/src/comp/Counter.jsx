3a258b7ec57023574397f8ff085d99d12acaaf05
Priyanshi-Chauhan <priyanshichauhanx06@gmail.com>
2019-12-29 16:30:39 +0530

    bootstrap changes

--- reacts/src/comp/Counter.jsx
+++ reacts/src/comp/Counter.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    imageUrl: "http://picsum.photos/200"
   };
   render() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <img src={this.state.imageUrl} alt=""></img>
+        <span style={{ fontSize: 20 }} className={classes}>
+          {this.formatCount()}
+        </span>
         <button className="btn btn-secondary btn-sm">increment</button>
       </div>
     );
