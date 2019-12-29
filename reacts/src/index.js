b505d15ff662b419bc45c40eef1834d691bfacac
Priyanshi-Chauhan <priyanshichauhanx06@gmail.com>
2019-12-29 14:46:33 +0530

    simple code

---
+++ reacts/src/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Counter from "./comp/Counter";
+import "bootstrap/dist/css/bootstrap.css";
+
+ReactDOM.render(<Counter></Counter>, document.getElementById("root"));
