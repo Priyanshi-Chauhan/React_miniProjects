39b64e6871ec16e210cbb754b525b5e46d8b2201
Priyanshi-Chauhan <priyanshichauhanx06@gmail.com>
2019-12-29 18:57:07 +0530

    adding components

--- reacts/src/comp/Kiki.jsx
+++ reacts/src/comp/Kiki.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import Counter from './Counter';
 class Kiki extends Component {
-    state = {  }
+    state = {
+        counters: [{ id: 1, value: 4 },
+        { id: 2, value: 0 },
+        { id: 3, value: 0 },
+            {id: 4, value: 0}]
+     }
     render() { 
         return (<div>
-            <Counter />
-            <Counter />
-            <Counter />
-            <Counter />
-
+            {this.state.counters.map(counter=><Counter key={counter.id} />)}
             
         </div>  
 
