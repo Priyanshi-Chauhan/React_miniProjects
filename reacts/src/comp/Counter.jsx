3c107ca175dbec72c0a44fcb097c3840ef4ed225
Priyanshi-Chauhan <priyanshichauhanx06@gmail.com>
2019-12-30 17:32:12 +0530

    much changes

--- reacts/src/comp/Counter.jsx
+++ reacts/src/comp/Counter.jsx
@@ -1,52 +1,55 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count: this.props.value,
+    value: this.props.counter.value
     //imageUrl: "http://picsum.photos/200",
     //tags: []
   };
   render() {
-    
-
     let classes = this.getBadgeClasses();
     return (
       <div>
-    
         <span style={{ fontSize: 20 }} className={classes}>
           {this.formatCount()}
         </span>
-        <button onClick = {() => this.handleIncrement()} className="btn btn-secondary btn-sm">Increment</button>
-        <button onClick = {() => this.props.onDelete(this.props.id)} className=" btn btn- primary btn-danger btm-sm m-3">Delete</button>     
-
+        <button
+          onClick={() => this.handleIncrement()}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className=" btn btn- primary btn-danger btm-sm m-3"
+        >
+          Delete
+        </button>
       </div>
     );
-  };
+  }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "ZERO" : count;
+    const { value } = this.state;
+    return value === 0 ? "ZERO" : value;
   }
   //renderTags() {
-    //if ((this.state.tags.length === 0)) return <p>there are no tags</p>;
-    //return (
-     // <ul>
-       // {this.state.tags.map(tag => (
-         // <li key={tag}>{tag}</li>
-       // ))}
-      //</ul>
-    //);
- // }
-  handleIncrement = () => { 
-    
-    this.setState({count:this.state.count + 1}) 
-  }
-  
-    
+  //if ((this.state.tags.length === 0)) return <p>there are no tags</p>;
+  //return (
+  // <ul>
+  // {this.state.tags.map(tag => (
+  // <li key={tag}>{tag}</li>
+  // ))}
+  //</ul>
+  //);
+  // }
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
 }
 
 export default Counter;
--- reacts/src/comp/Kiki.jsx
+++ reacts/src/comp/Kiki.jsx
@@ -1,25 +1,48 @@
-import React, { Component } from 'react';
-import Counter from './Counter';
+import React, { Component } from "react";
+import Counter from "./Counter";
 class Kiki extends Component {
-    state = {
-        counters: [{ id: 1, value: 4 },
-        { id: 2, value: 0 },
-        { id: 3, value: 0 },
-        { id: 4, value: 0 }]
-    }
-    render() {
-        return (<div>
-            {this.state.counters.map(counter => <Counter key={counter.id} onDelete={this.handleDelete} value={counter.value} id={counter.id} />)}
-            
-        </div>
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+  render() {
+    return (
+      <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+        {this.state.counters.map(counter => (
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            counter={counter}
+          />
+        ))}
+      </div>
+    );
+  }
 
-        );
-    }
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+  handleIncrement = counter => {
+    const counters = [...this.props.counters];
+    const index = counters.indexOf(counter);
+    counter[index] = { ...counter };
+    counter[index].value++;
+    this.setState({ counters });
+  };
 
-    handleDelete = (counterId) => {
-        const counters = this.state.counters.filter(c => c.id !== counterId);
-        this.setState({ counters });
-    };
+  handleReset = () => {};
 }
- 
+
 export default Kiki;
