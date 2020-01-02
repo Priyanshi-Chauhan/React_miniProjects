0f239de437517444ef3fa5efb90476653cdbf65e
Priyanshi-Chauhan <priyanshichauhanx06@gmail.com>
2020-01-03 02:11:51 +0530

    changes

--- songs/src/Compo.js
+++ songs/src/Compo.js
@@ -3,7 +3,22 @@ import ReactDOM from "react-dom";
 class Compo extends Component {
   state = {};
   render() {
-    return <div>hi there</div>;
+    return (
+      <div>
+        <div>
+          <a href="/">
+            <img alt="avatar"></img>
+          </a>
+          <div>
+            <a href="/">sam</a>
+            <div>
+              <span>today at 6pm </span>
+            </div>
+            <div>what a nice blog </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 export default Compo;
