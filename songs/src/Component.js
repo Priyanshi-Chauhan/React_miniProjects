db48e5125f86038be3d11e7e41b5dc93cc1cc217
Priyanshi-Chauhan <priyanshichauhanx06@gmail.com>
2020-01-03 00:38:37 +0530

    comment sections

---
+++ songs/src/Component.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Component extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <div>
+          <img alt="avatar" src="">
+            {" "}
+            <a href="/"></a>
+          </img>
+        </div>
+        <div className="hii">sam</div>
+        <div>today at 6.00 pm </div>
+      </div>
+    );
+  }
+}
+
+export default Component;
---
+++ songs/src/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+//import Component from "./Components";
+class App extends Component {
+    state = {}
+    render() {
+        return (
+            <Component />)
+    };
+
+    exports App.default;
+}
+ReactDOM.render(< App />, document.getElementById("root"));
