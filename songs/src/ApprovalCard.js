870e47af3e4f308407bef18ad4d7a048487b57f3
Priyanshi-Chauhan <priyanshichauhanx06@gmail.com>
2020-01-03 14:55:02 +0530

    component reusability

--- songs/src/ApprovalCard.js
+++ songs/src/ApprovalCard.js
@@ -2,14 +2,13 @@ import React, { Component } from "react";
 const ApprovalCard = props => {
   return (
     <div className="ui card">
-      <div className="content"></div>
+      <div className="content">{props.children}</div>
       <div className="extra content">
         <div className="ui two buttons">
           <div className=" ui basic green button">ACCEPT</div>
           <div className="ui basic red button">REJECT</div>
         </div>
       </div>
-      {props.children}
     </div>
   );
 };
--- songs/src/index.js
+++ songs/src/index.js
@@ -8,12 +8,16 @@ const App = () => {
       <ApprovalCard>
         <Compo person="Sam" timeAgo="today at 5 pm " blog="yo man"></Compo>
       </ApprovalCard>
-      <Compo
-        person="Alex"
-        timeAgo="yesterday at 12 noon "
-        blog="hiii you there!!"
-      />
-      <Compo person="Susu" timeAgo="yesterday at 5 pm " blog="i am gud" />
+      <ApprovalCard>
+        <Compo
+          person="Alex"
+          timeAgo="yesterday at 12 noon "
+          blog="hiii you there!!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <Compo person="Susu" timeAgo="yesterday at 5 pm " blog="i am gud" />
+      </ApprovalCard>
     </div>
   );
 };
