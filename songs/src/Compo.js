0da199b4c65159765be27d9eb7a238814dc1d6c7
Priyanshi-Chauhan <priyanshichauhanx06@gmail.com>
2020-01-03 10:14:04 +0530

    comment section

--- songs/src/Compo.js
+++ songs/src/Compo.js
@@ -1,21 +1,22 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 class Compo extends Component {
   state = {};
   render() {
     return (
-      <div>
-        <div>
-          <a href="/">
-            <img alt="avatar"></img>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            sam
           </a>
-          <div>
-            <a href="/">sam</a>
-            <div>
-              <span>today at 6pm </span>
-            </div>
-            <div>what a nice blog </div>
+          <div className="metadata">
+            <span>today at 6pm </span>
           </div>
+          <div>what a nice blog </div>
         </div>
       </div>
     );
--- songs/src/index.js
+++ songs/src/index.js
@@ -1,5 +1,17 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Compo from "./Compo";
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <Compo />
+      <Compo />
+      <Compo />
+      <Compo />
+      <Compo />
+      <Compo />
+    </div>
+  );
+};
 
-ReactDOM.render(<Compo />, document.getElementById("root"));
+ReactDOM.render(<App />, document.querySelector("#root"));
