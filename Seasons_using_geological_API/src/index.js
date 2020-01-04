d6cd2d9a32f93b2e145f8d1aa56ecf53b72becbd
Priyanshi-Chauhan <priyanshichauhanx06@gmail.com>
2020-01-04 22:25:54 +0530

    success

---
+++ Seasons_using_geological_API/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null };
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => console.log("error")
+    );
+  }
+  render() {
+    return <div>latitude: {this.state.lat}</div>;
+  }
+}
+ReactDOM.render(<App />, document.getElementById("root"));
