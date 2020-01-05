08dc01932a477dc852ba020e0896fa57ab838d5c
Priyanshi-Chauhan <priyanshichauhanx06@gmail.com>
2020-01-05 20:07:02 +0530

    success

--- Seasons_using_geological_API/src/SeasonDisplay.js
+++ Seasons_using_geological_API/src/SeasonDisplay.js
@@ -12,22 +12,22 @@ const seasonConfig = {
 const GetSeason = (lat, month) => {
   if (month > 3 && month < 9) {
     return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
   }
-  if (month < 3 && month > 9) {
-    return lat < 0 ? "winter" : "summer";
-  }
-  const SeasonDisplay = props => {
-    const season = GetSeason(props.lat, new Date.getMonth());
-
-    const { text, iconName } = seasonConfig[season];
-
-    return (
-      <div>
-        <i className={`${iconName} icon`}></i>
-        <h4>{text}</h4>
-        <i className={`${iconName} icon`}></i>
-      </div>
-    );
-  };
-  export default SeasonDisplay;
 };
+const SeasonDisplay = props => {
+  const season = GetSeason(props.lat, new Date().getMonth());
+
+  const { text, iconName } = seasonConfig[season];
+
+  return (
+    <div>
+      <i className={`${iconName} icon`}></i>
+      <h4>{text}</h4>
+      <i className={`${iconName} icon`}></i>
+    </div>
+  );
+};
+
+export default SeasonDisplay;
